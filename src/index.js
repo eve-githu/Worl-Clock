9b6c9db5716079a558fad1ee14e7509de0a12f9b
evegithu-hue <evegithu@gmail.com>
2026-06-23 17:26:44 +0200

    Added a link back to all cities

--- src/index.js
+++ src/index.js
@@ -58,6 +58,8 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}</div>
         </div>
+
+        <a href="index.html"> All cities </a>
         `;
 }
 
